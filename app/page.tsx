df5478faab69487a3f28d30fad33f149b87fcda9
Igor Occon <occon24@gmail.com>
2024-08-08 11:58:07 -0300

    stop: manipulation on db name and image, barber

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,14 @@ import Image from "next/image"
 import { Card, CardContent } from "./_components/ui/card"
 import { Badge } from "./_components/ui/badge"
 import { Avatar, AvatarImage } from "./_components/ui/avatar"
+import { db } from "./_lib/prisma"
+import BarbershopItem from "./_components/barbershop-item"
+
 //SERVER COMPONENTS
-const Home = () => {
+const Home = async () => {
+  // chamar meu banco de dados
+  const barbershops = await db.barbershop.findMany({})
+
   return (
     <div>
       <Header />
@@ -37,29 +43,40 @@ const Home = () => {
 
         {/* AGENDAMENTO */}
 
-        <div>
-          <Card className="mt-6">
-            <CardContent className="flex justify-between p-0">
-              {/* ESQUERDA */}
-              <div className="flex flex-col gap-2 py-5 pl-5">
-                <Badge className="w-fit">Confirmado</Badge>
-                <h3 className="font-semibold">Corte de Cabelo</h3>
-                <div className="flex items-center gap-2">
-                  <Avatar className="h-6 w-6">
-                    <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
-                  </Avatar>
-                  <p className="text-sm">Barbearia FSW</p>
-                </div>
-              </div>
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Agendamentos
+        </h2>
 
-              {/* DIREITA */}
-              <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
-                <p className="text-sm">Agosto</p>
-                <p className="text-2xl">08</p>
-                <p className="text-sm">20:00</p>
+        <Card>
+          <CardContent className="flex justify-between p-0">
+            {/* ESQUERDA */}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de Cabelo</h3>
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+                </Avatar>
+                <p className="text-sm">Barbearia FSW</p>
               </div>
-            </CardContent>
-          </Card>
+            </div>
+
+            {/* DIREITA */}
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">08</p>
+              <p className="text-sm">20:00</p>
+            </div>
+          </CardContent>
+        </Card>
+
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Recomendados
+        </h2>
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
         </div>
       </div>
     </div>
