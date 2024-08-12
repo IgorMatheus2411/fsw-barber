9ba4f35b66fe3981d5ece1f4e18ffc73767fd328
Igor <occon24@gmail.com>
2024-08-11 23:36:44 -0300

    feat: ensure barbershops can be searched by title and service

--- app/barbershops/page.tsx
+++ app/barbershops/page.tsx
@@ -3,19 +3,71 @@ import Header from "../_components/header"
 import Search from "../_components/search"
 import { db } from "../_lib/prisma"
 
+// pesquisar pelo nome da barbearia e pelo servico
+// interface BarbershopsPageProps {
+//   searchParams: {
+//     search?: string
+//   }
+// }
+
+// pesquisar pelo servico
+// const BarbershopsPage = async ({ searchParams }: BarbershopsPageProps) => {
+//   const barbershops = await db.barbershop.findMany({
+//     where: {
+//      OR: [
+//       {
+//         name: {
+//           contains: searchParams?.search,
+//           mode: "insensitive",
+//         },
+//       },
+//       {
+//         services: {
+//           some: {
+//            name: {
+//              contains: searchParams?.search,
+//              mode: "insensitive"
+//            },
+//           },
+//         },
+//       },
+//      ],
+//     },
+//   })
+
 interface BarbershopsPageProps {
   searchParams: {
-    search?: string
+    title?: string
+    service?: string
   }
 }
 
 const BarbershopsPage = async ({ searchParams }: BarbershopsPageProps) => {
   const barbershops = await db.barbershop.findMany({
     where: {
-      name: {
-        contains: searchParams?.search,
-        mode: "insensitive",
-      },
+      OR: [
+        searchParams?.title
+          ? {
+              name: {
+                contains: searchParams?.title,
+                mode: "insensitive",
+              },
+            }
+          : {},
+
+        searchParams.service
+          ? {
+              services: {
+                some: {
+                  name: {
+                    contains: searchParams?.title,
+                    mode: "insensitive",
+                  },
+                },
+              },
+            }
+          : {},
+      ],
     },
   })
 
@@ -28,7 +80,8 @@ const BarbershopsPage = async ({ searchParams }: BarbershopsPageProps) => {
 
       <div className="px-5">
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
-          Resultados para &quot;{searchParams.search}&quot;
+          Resultados para &quot;{searchParams.title || searchParams?.service}
+          &quot;
         </h2>
 
         <div className="grid grid-cols-2 gap-4">
